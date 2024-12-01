d15fe2cd0a8481de5b0b3b2f871e411363bee199
Oleg-art-cyber <olegperm@icloud.com>
2024-12-01 09:17:30 +0200

    Update ex1.js

--- hw/ex1.js
+++ hw/ex1.js
@@ -1,14 +1,14 @@
-const num3 = 30; //3
+const num = 30; //3
 
 Number(num % 2 === 0 || num % 3 === 0 || num % 5 === 0) &&
-  !(num % 2 === 0 && num % 3 === 0 || num % 3 === 0 && num % 5 === 0 || num % 5 === 0 && num % 2 === 0) &&
-  !(num % 2 === 0 && num % 3 === 0 && num % 5 === 0) &&
-  console.log('1');
+!(num % 2 === 0 && num % 3 === 0 || num % 3 === 0 && num % 5 === 0 || num % 5 === 0 && num % 2 === 0) &&
+!(num % 2 === 0 && num % 3 === 0 && num % 5 === 0) &&
+console.log('1');
 
-Number(num2 % 2 === 0 && num2 % 3 === 0 || num2 % 3 === 0 && num2 % 5 === 0 || num2 % 5 === 0 && num2 % 2 === 0) &&
-  !(num2 % 2 === 0 && num2 % 3 === 0 && num2 % 5 === 0) &&
-  console.log('2');
+Number(num % 2 === 0 && num % 3 === 0 || num % 3 === 0 && num % 5 === 0 || num % 5 === 0 && num % 2 === 0) &&
+!(num % 2 === 0 && num % 3 === 0 && num % 5 === 0) &&
+console.log('2');
 
 
-Number(num3 % 2 === 0 && num3 % 3 === 0 && num3 % 5 === 0) &&
-  console.log('3');
+Number(num % 2 === 0 && num % 3 === 0 && num % 5 === 0) &&
+console.log('3');
